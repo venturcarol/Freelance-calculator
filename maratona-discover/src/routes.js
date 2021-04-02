7d360ae440ce2fa4dda303df58dbf936e87b984f
Carolina Ventura <carolventura1996@gmail.com>
2021-04-02 14:47:17 +0100

    using ejs

--- maratona-discover/src/routes.js
+++ maratona-discover/src/routes.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const routes = express.Router() 
+const routes = express.Router()
+const views = __dirname + "/views/"
 
-const basePath = __dirname + "/views"
+const profile = {
+    name: "Carolina",
+    avatar: "https://avatars.githubusercontent.com/u/62064102?v=4",
+    "monthly-budget": 3000,
+    "days-per-week": 5, 
+    "hours-per-day": 5, 
+    "vacation-per-year": 4
+}
 
-routes.get('/', (request, response) => response.sendFile(basePath + "/index.html"))
-routes.get('/job', (request, response) => response.sendFile(basePath + "/job.html"))
-routes.get('/job/edit', (request, response) => response.sendFile(basePath + "/job-edit.html"))
-routes.get('/profile', (request, response) => response.sendFile(basePath + "/profile.html"))
-
+routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/job', (req, res) => res.render(views + "job"))
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
+ 
 module.exports = routes;
