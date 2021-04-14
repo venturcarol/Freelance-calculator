1baf147dbd51b3ddf7202063cde79f1b136d324e
Carolina Ventura <carolventura1996@gmail.com>
2021-04-14 23:43:02 +0100

    creating database

--- maratona-discover/src/controllers/JobController.js
+++ maratona-discover/src/controllers/JobController.js
@@ -11,13 +11,14 @@ module.exports = {
       const jobs = Job.get()
       const lastId = jobs[jobs.length - 1]?.id || 0;
 
-      jobs.push({
-          id: lastId + 1,
-          name: req.body.name,
-          "daily-hours": req.body["daily-hours"],
-          "total-hours": req.body["total-hours"],
-          created_at: Date.now()
-      })
+      Job.create({
+        id: lastId + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now()
+    });
+
       return res.redirect('/')
     },
     show(req, res) {
---
+++ maratona-discover/src/db/init.js
@@ -0,0 +1,16 @@
+const Database = require('config')
+
+Database()
+
+Database.exec(`CREATE TABLE profile (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    avatar TEXT,
+    monthly-budget INT,
+    days-per-week INT,
+    hours-per-day INT, 
+    vacation-per-year INT,
+    value-hour INT 
+)`);
+
+Database.close()
