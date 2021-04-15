98ed037dbdd783d99dcfdb30a636ae2573bbfb09
Carolina Ventura <carolventura1996@gmail.com>
2021-04-15 23:24:44 +0100

    implementing async and await

--- maratona-discover/src/db/init.js
+++ maratona-discover/src/db/init.js
@@ -1,16 +1,71 @@
-const Database = require('config')
+const Database = require('./config')
 
-Database()
+const initDb = {
+    async init(){
 
-Database.exec(`CREATE TABLE profile (
+const db = await Database()
+
+await db.exec(`CREATE TABLE profile (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     avatar TEXT,
-    monthly-budget INT,
-    days-per-week INT,
-    hours-per-day INT, 
-    vacation-per-year INT,
-    value-hour INT 
+    monthly_budget INT,
+    days_per_week INT,
+    hours_per_day INT, 
+    vacation_per_year INT,
+    value_hour INT 
 )`);
 
-Database.close()
+await db.exec(`CREATE TABLE jobs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    daily_hours INT,
+    total_hours INT, 
+    created_at DATETIME
+)`)
+
+await db.run(`INSERT INTO profile (
+    name, 
+    avatar, 
+    monthly_budget, 
+    days_per_week, 
+    hours_per_day, 
+    vacation_per_year
+) VALUES (
+    "jakeliny",
+    "https://avatars.githubusercontent.com/u/17316392",
+    3000,
+    5,
+    5,
+    4
+);`)
+
+await db.run(`INSERT INTO jobs (
+    name,
+    daily_hours,
+    total_hours,
+    created_at
+) VALUES (
+    "Pizzaria Guloso",
+    2,
+    1,
+    1617514376018
+);`)
+
+await db.run(`INSERT INTO jobs (
+    name,
+    daily_hours,
+    total_hours,
+    created_at
+) VALUES (
+    "OneTwo Projects",
+    3,
+    47,
+    1617514376018
+);`)
+
+await db.close()
+    }
+}
+
+initDb.init()
